30fd881ce96977f85e3c80223582a2ff6d0b080e
sean may <san.quentin.may@gmail.com>
2024-06-29 17:32:26 -0700

    project 4 final

--- scripts/index.js
+++ scripts/index.js
@@ -1,56 +1,78 @@
-const cardData1 = {
-  name: "Yosemite Valley",
-  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
-};
-
-const cardData2 = {
-  name: "Lake Louise",
-  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
-};
-
-const cardData3 = {
-  name: "Bald Mountains",
-  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
-};
-
-const cardData4 = {
-  name: "Latemar",
-  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
-};
-
-const cardData5 = {
-  name: "Vanoise National Park",
-  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
-};
-
-const cardData6 = {
-  name: "Lago di Braies",
-  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
-};
-
 const initialCards = [
-  cardData1,
-  cardData2,
-  cardData3,
-  cardData4,
-  cardData5,
-  cardData6,
+  {
+    name: "Yosemite Valley",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+  },
+  {
+    name: "Lake Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+  },
 ];
 
+// Elements
+
 const profileEditBtn = document.querySelector(".profile__edit-button");
-
-let modal = document.querySelector(".modal");
-
+const modal = document.querySelector(".modal");
 const closeButton = document.querySelector(".modal__close");
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
 
-function openEdit() {
-  modal.classList.add("modal__open");
+const form = modal.querySelector(".modal__form");
+const modalTitle = modal.querySelector(".modal__title-input");
+const modalDescription = modal.querySelector(".modal__description-input");
+
+const cardList = document.querySelector(".cards__list");
+const cardTemplate =
+  document.querySelector("#card__template").content.firstElementChild;
+
+// Functions
+
+function toggleEdit() {
+  modal.classList.toggle("modal__open");
+  modalTitle.value = profileTitle.textContent;
+  modalDescription.value = profileDescription.textContent;
 }
 
-profileEditBtn.addEventListener("click", openEdit);
-
-function closeEdit() {
-  modal.classList.remove("modal__open");
+function profileSave(event) {
+  event.preventDefault();
+  profileTitle.textContent = modalTitle.value;
+  profileDescription.textContent = modalDescription.value;
+  toggleEdit();
 }
 
-closeButton.addEventListener("click", closeEdit);
+function getCardElement(cardData) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImage = cardElement.querySelector(".cards__image");
+  const cardTitle = cardElement.querySelector(".cards__title");
+  cardImage.src = cardData.link;
+  cardTitle.textContent = cardData.name;
+  cardImage.textContent = cardData.name;
+  return cardElement;
+}
+
+// Listeners
+
+profileEditBtn.addEventListener("click", toggleEdit);
+closeButton.addEventListener("click", toggleEdit);
+form.addEventListener("submit", profileSave);
+
+initialCards.forEach((cardData) => {
+  const cardElement = getCardElement(cardData);
+  cardList.append(cardElement);
+});
