b46fe8ae94937a44bd5224c48462cc5f4e913e6a
sean may <san.quentin.may@gmail.com>
2024-06-30 17:43:39 -0700

    project 4 final

--- scripts/index.js
+++ scripts/index.js
@@ -34,8 +34,8 @@ const profileTitle = document.querySelector(".profile__title");
 const profileDescription = document.querySelector(".profile__description");
 
 const form = modal.querySelector(".modal__form");
-const modalTitle = modal.querySelector(".modal__title-input");
-const modalDescription = modal.querySelector(".modal__description-input");
+const modalTitle = modal.querySelector(".modal__title");
+const modalDescription = modal.querySelector(".modal__description");
 
 const cardList = document.querySelector(".cards__list");
 const cardTemplate =
@@ -43,13 +43,17 @@ const cardTemplate =
 
 // Functions
 
-function toggleEdit() {
-  modal.classList.toggle("modal__open");
+function fillProfileModal() {
   modalTitle.value = profileTitle.textContent;
   modalDescription.value = profileDescription.textContent;
 }
 
-function profileSave(event) {
+function toggleEdit() {
+  fillProfileModal();
+  modal.classList.toggle("modal_open");
+}
+
+function saveProfile(event) {
   event.preventDefault();
   profileTitle.textContent = modalTitle.value;
   profileDescription.textContent = modalDescription.value;
@@ -62,7 +66,7 @@ function getCardElement(cardData) {
   const cardTitle = cardElement.querySelector(".cards__title");
   cardImage.src = cardData.link;
   cardTitle.textContent = cardData.name;
-  cardImage.textContent = cardData.name;
+  cardImage.alt = cardData.name;
   return cardElement;
 }
 
@@ -70,7 +74,7 @@ function getCardElement(cardData) {
 
 profileEditBtn.addEventListener("click", toggleEdit);
 closeButton.addEventListener("click", toggleEdit);
-form.addEventListener("submit", profileSave);
+form.addEventListener("submit", saveProfile);
 
 initialCards.forEach((cardData) => {
   const cardElement = getCardElement(cardData);
